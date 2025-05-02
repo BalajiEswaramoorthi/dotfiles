41d7ffb561267bf845fe4229350ce6c085d5f0c3
Balaji Eswaramoorthi <balajisubasheckp@outlook.com>
2025-05-02 12:42:24 +0000

    dotfiles: neovim configurations

---
+++ nvim/lua/keymaps.lua
@@ -0,0 +1,35 @@
+vim.g.mapleader = " " -- leader key
+
+function toggleMouse()
+	if vim.o.mouse == "a" then
+		vim.o.mouse = ""
+		print "toggleMouse: Mouse Disabled"
+	else
+		vim.o.mouse = "a"
+		print "toggleMouse: Mouse Enabled"
+	end
+end
+
+function toggle_terminal()
+    for i, buffer in ipairs(vim.api.nvim_list_bufs()) do
+        local buffer_name = vim.api.nvim_buf_get_name(buffer)
+        if (string.sub(buffer_name, 1, 7) == "term://") then
+            vim.api.nvim_win_set_buf(0, buffer)
+            return
+        end
+    end
+    vim.api.nvim_command(":terminal")
+end
+
+vim.keymap.set("n", "<leader>m", ":lua toggleMouse()<CR>", { noremap = true, silent = true, desc = "Toggle mouse (Enable/Disable)" })
+
+vim.keymap.set("n", "<leader>sc", ":close<CR>", { noremap = true, silent = true, desc = "close the current window" })
+
+vim.keymap.set("n", "<leader>cmd", ":lua toggle_terminal()<CR>", { noremap = true, silent = true, desc = "Opens terminal in a buffer"})
+
+vim.keymap.set(
+	"n",
+	"<leader>q",
+	":quitall<CR>",
+	{ noremap = true, silent = true, desc = "Quit all windows" }
+)
